dfd223f13df288fabed8957c184d606317307108
Joonas Partanen <partanen.joonas@gmail.com>
2021-10-19 11:57:46 +0300

    Submit exercise 12.9

--- todo-app/todo-backend/util/config.js
+++ todo-app/todo-backend/util/config.js
@@ -1,7 +1,7 @@
 const MONGO_URL = process.env.MONGO_URL || 'mongodb://root:example@localhost:3456/the_database'
-const REDIS_URL = process.env.REDIS_URL || undefined
+const REDIS_URL = process.env.REDIS_URL || 'redis://localhost:6379'
 
 module.exports = {
   MONGO_URL,
-  REDIS_URL//: '//localhost:6378'
+  REDIS_URL,
 }
